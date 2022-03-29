4027c7b4a30244ef8219a45d63f2457ea2b7d097
Bryan lee <leeweihan98@yahoo.com.sg>
2022-03-29 21:35:06 +0800

    Converting database to csv files

--- Testing.py
+++ Testing.py
@@ -11,7 +11,7 @@ db_connection = mysql.connect(user="root", password="jajasauce", host="34.143.21
 cur = db_connection.cursor()
 
 
-#cur.execute("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES")
+cur.execute("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES")
 
 #cur.execute("SELECT * FROM Player_Summaries LIMIT 3;")
 #cur.execute("SELECT * FROM App_ID_Info;")
@@ -19,13 +19,20 @@ cur = db_connection.cursor()
     
 #What i want from the dataset , Get from 1st table : List of games App_ID_Info (List of all games on steam) 
 # Merge 2 tables (App_ID_Info) and (GAMES_GENRES) to get the genre of games
-cur.execute("SELECT ap.appid,ap.title,ap.price,gg.genre FROM App_ID_Info ap INNER JOIN Games_Genres gg ON ap.appid = gg.appid WHERE ap.type = 'game';")
+#cur.execute("SELECT ap.appid,ap.title,ap.price,gg.genre FROM App_ID_Info ap INNER JOIN Games_Genres gg ON ap.appid = gg.appid WHERE ap.type = 'game';")
 
-myresult = cur.fetchall()
-df = pd.DataFrame(myresult,columns=['appid','title','price','genre'])
-print(df['genre'].value_counts())
+#myresult = cur.fetchall()
+#df = pd.DataFrame(myresult,columns=['appid','title','price','genre'])
+#print(df['genre'].value_counts())
 
 
 #for x in myresult:
 
 #    print(x) #print all tables
+
+#Now we get the user info, games they played , hours played, groups joined . Inner join 4 tables using SQL (DATA SET TOO BIG)
+#cur.execute("SELECT ps.steamid,g1.appid,ap.title,ap.price,g1.playtime_forever FROM Player_Summaries ps INNER JOIN Games_1 g1 ON g1.steamid = ps.steamid INNER JOIN App_ID_Info ap ON g1.appid = ap.appid  WHERE ap.type = 'game' AND g1.playtime_forever != 'None' LIMIT 10000")
+
+myresult = cur.fetchall()
+for x in myresult:
+    print(x) #print all tables
